4eebea83938bc3c3584f465e1a0948f400c9a62d
kchenna <kamalc.inbox@gmail.com>
2017-07-08 08:08:24 -0500

    removed kc url

--- src/main/java/com/cks/api/movies/MovieApi.java
+++ src/main/java/com/cks/api/movies/MovieApi.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/kc/tamil")
+@RequestMapping("/tamil")
 public class MovieApi {
 	
 	@Autowired
