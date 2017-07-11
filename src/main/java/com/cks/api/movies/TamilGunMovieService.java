ae4cead3a91477ed1ca4bd5c6363025d7da9861d
DS\chennakk <kamalakannan.chennakesavan@cdk.com>
2017-07-11 17:18:18 -0500

    service added

--- src/main/java/com/cks/api/movies/TamilGunMovieService.java
+++ src/main/java/com/cks/api/movies/TamilGunMovieService.java
@@ -16,10 +16,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@Service
 public class TamilGunMovieService implements MovieService {
 
 	private static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36";
